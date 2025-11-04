bef0a7c629c634d43556bc67497f715e16f9325b
mukuljaiswal <mukuljaiswal282@gmail.com>
2025-11-04 23:05:24 +0530

    sixth-commit

--- js/app.js
+++ js/app.js
@@ -28,3 +28,22 @@ $('#testimonial-slider').owlCarousel({
         }
     }
 })
+$('#blog-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:4
+        }
+    }
+})
