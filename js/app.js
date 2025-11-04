2b0d92d3925238eeb0501d206df51adc963453c0
mukuljaiswal <mukuljaiswal282@gmail.com>
2025-11-04 21:42:42 +0530

    fivth-commit

--- js/app.js
+++ js/app.js
@@ -9,3 +9,22 @@ window.addEventListener("scroll",()=>{
         header.classList.remove("headerScroll")
     }
 })
+$('#testimonial-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
